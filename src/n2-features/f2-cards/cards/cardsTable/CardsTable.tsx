0de82e642b73de657f585292f0a05a6a4856c7c9
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-04-20 21:51:51 +0300

    deletev1

---
+++ src/n2-features/f2-cards/cards/cardsTable/CardsTable.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {CardType} from '../../../../n1-main/m3-dal/m1-API/cardsAPI';
+import {Card} from './Card';
+import packsS from "./CardsTable.module.css";
+
+
+
+export type CardsTablePropsType = {
+    cards: CardType []
+}
+
+export const CardsTable: React.FC<CardsTablePropsType> = ({cards}) => {
+
+    return (
+        <div>
+            <div className={packsS.tableHeaderWrapper}>
+                <div className={packsS.tableHeader}>
+                    <div >
+                        Question
+                    </div>
+
+                    <div >
+                        Answer
+                    </div>
+
+                    <div >
+                        Last Updated
+                    </div>
+
+                    <div>
+                        Grade
+                    </div>
+
+                    <div>
+                        Actions
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <div style={{height: '440px'}}>
+                    {cards && cards.map(card => <Card key={card._id} card={card}/>)}
+                </div>
+            </div>
+        </div>
+    );
+};

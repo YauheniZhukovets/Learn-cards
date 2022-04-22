79d13ff05b327a051614fc9237726d5ec56fbb75
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2022-04-22 15:32:02 +0300

    fix error StarRating

--- src/n2-features/f2-cards/cards/cardsTable/StarRating.tsx
+++ src/n2-features/f2-cards/cards/cardsTable/StarRating.tsx
@@ -1,18 +1,19 @@
 import s from './Card.module.css'
 import {FaStar} from 'react-icons/fa';
 
-export default function StarRating({ numTotalStars = 5, initialRating = 0 }) {
+export default function StarRating({numTotalStars = 5, initialRating = 0}) {
     const numSelectedStars = initialRating
 
-    const getColor=( i:any, numSelectedStars:number)=> {
-        return i < numSelectedStars ? "rgba(190, 47, 65)" : " #5b5858";
+    const getColor = (i: any, numSelectedStars: number) => {
+        return i < numSelectedStars ? 'rgba(190, 47, 65)' : ' #5b5858';
     }
 
     return (
         <div className={s.starRating}>
             <div>
-                {Array.from({ length: numTotalStars }).map((e, i) => (
-                    <FaStar className={s.starRatingStar}
+                {Array.from({length: numTotalStars}).map((e, i) => (
+                    <FaStar key={i}
+                            className={s.starRatingStar}
                             color={getColor(i, numSelectedStars)}
                     />
                 ))}
